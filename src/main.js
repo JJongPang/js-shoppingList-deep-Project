e2fc599a145271f81078b0771eec39d90596d856
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2020-12-15 19:58:22 +0900

    main.js update

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 //Fetch Json file
 function loadItems() {
     return fetch('data/data.json')
-    .then(response => response.json())
-    .then(json => json.items);
+        .then((response) => response.json())
+        .then((json) => json.items);
 }
 
-//Update item
 function displayItems(items) {
     const container = document.querySelector('.items');
-    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+    container.innerHTML = items.map((item) => createHTMLString(item)).join('');
 }
 
-//Create HTML list
 function createHTMLString(item) {
     return `
     <li class="item">
@@ -35,33 +33,21 @@ function setEventListeners(items) {
 function onButtonClick(event, items) {
     const dataset = event.target.dataset;
     const key = dataset.key;
-    const value = dataset.value;    
-    
-    if(key == null || value == null) {
+    const value = dataset.value;
+
+    if (key == null || value == null) {
         return;
     }
 
     //updateItems(items, key, value);
-    displayItems(items.filter(item => item[key] === value));
+    displayItems(items.filter((item) => item[key] === value));
 }
 
-//Make the items matching
-// function updateItems(items, key, value) {
-//     items.forEach(item => {
-//         if(item.dataset[key] === value) {
-//             item.classList.remove('invisible');
-//         }else {
-//             item.classList.add('invisble');
-//         }
-//     });
-// }
-
-//main
 loadItems()
-    .then(items => {
+    .then((items) => {
         displayItems(items);
         setEventListeners(items);
     })
-    .catch(err => {
+    .catch((err) => {
         console.error(err);
     });
